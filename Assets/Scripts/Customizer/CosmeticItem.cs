ca9bae7056f3f031593690ecf8da2da5cf5f3902
Bupathi Sangeeth <bswonline.mailme@gmail.com>
2023-12-17 22:37:27 +0530

    setup the UI structure for cosmetic tabs

--- Assets/Scripts/Customizer/CosmeticItem.cs
+++ Assets/Scripts/Customizer/CosmeticItem.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [System.Serializable]
 public class CosmeticItem
 {
-    [DoNotSerialize]
+    [System.NonSerialized]
     public int TypeId;
     public int ItemId;
     public int Price;
---
+++ Assets/Scripts/UI/UITabBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITabBar : MonoBehaviour
+{
+    [SerializeField]
+    private List<UITab> Tabs;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
